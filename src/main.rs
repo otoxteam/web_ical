87038bae5f9af8525526fadb434ffc33081c0c4d
Dayrel Negrin <otoxteam@gmail.com>
2019-07-10 01:50:57 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ extern crate web_ical;
 use web_ical::Calendar;
 
 fn main() {
-    let icals = Calendar::new("http://127.0.0.1/basic.ics");
-    //http://ical.mac.com/ical/US32Holidays.ics
+    let icals = Calendar::new("http://ical.mac.com/ical/US32Holidays.ics");
     for ical in &icals.events{
          println!("Event: {}", ical.summary);
          println!("Started: {}", ical.dtsart.format("%a, %e %b %Y - %T"));
