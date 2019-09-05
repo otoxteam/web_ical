a4e1c4488b4b78f435fe0a4f29f4304934211cee
Ward Segers <w@rdsegers.be>
2019-09-05 21:32:21 +0200

    Bench bugfixes

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,15 +1,18 @@
+#![feature(test)]
+
 extern crate test;
+extern crate web_ical;
 
 #[bench]
 fn export_ics(bencher: &mut test::Bencher) {
-    use crate::Calendar;
+    use web_ical::Calendar;
     let ical = Calendar::new("https://gist.githubusercontent.com/DeMarko/6142417/raw/1cd301a5917141524b712f92c2e955e86a1add19/sample.ics");
     bencher.iter(|| ical.export_ics("/tmp/1"))
 }
 
 #[bench]
 fn export_writer(bencher: &mut test::Bencher) {
-    use crate::Calendar;
+    use web_ical::Calendar;
     let ical = Calendar::new("https://gist.githubusercontent.com/DeMarko/6142417/raw/1cd301a5917141524b712f92c2e955e86a1add19/sample.ics");
     bencher.iter(|| {
         let mut file = std::fs::File::create("/tmp/2").unwrap();
